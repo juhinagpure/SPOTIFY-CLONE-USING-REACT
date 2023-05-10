99d6f69bbe8a15fd71b942bd06b62c7a00f3367e
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-05-10 23:49:31 +0530

    added new classes

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -5,6 +5,7 @@ export const initialState = {
   playlists: [],
   userInfo: null,
   selectedPlaylistId: "7ny12nsbuhFQr1r3VXzrZ6",
+  selectedPlaylist: null,
 };
 
 const reducer = (state, action) => {
@@ -25,6 +26,13 @@ const reducer = (state, action) => {
       return {
         ...state,
         userInfo: action.userInfo,
+      };
+    }
+
+    case reducerCases.SET_PLAYLIST: {
+      return {
+        ...state,
+        selectedPlaylist: action.selectedPlaylist,
       };
     }
 
